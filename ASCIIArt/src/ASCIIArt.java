88f8a0031d5700a9d58a8336f66add164ab20be5
s-fernandoa <s-fernandoa@53S50030603.it.bsd405.org>
2018-09-20 13:13:34 -0700

    Ayyye

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -14,4 +14,123 @@
  *
  */
 
-// YOUR CODE GOES HERE :)
+
+public class ASCIIArt {
+	public static void main(String[] args) {
+		sun();
+		sun();
+		sun();
+		System.out.println("ON VERY BRIGHT SUNNY DAYS...\n");
+		letterP();
+		letterO();
+		letterP();
+		letterS();
+		letterI();
+		letterC();
+		letterL();
+		letterE();
+		letterS();
+		System.out.println("...ARE A GREAT CHOICE!\n");
+		top();
+		bottom();
+		popsicle();
+		popsicle();
+		System.out.print("YUM!");
+	}
+	public static void top() {
+		System.out.println(" -----     ---");
+		System.out.println("/     \\    | |");
+		System.out.println("|     |    | |");
+		System.out.print("|     |    | |\n");
+		System.out.println("|     |    | |");
+		System.out.println("|     |   -----");
+		
+	}
+	public static void bottom() {
+		System.out.println("|     |  |     |");
+		System.out.println(" -----   |     |");
+		System.out.println("  | |    |     |");
+		System.out.println("  | |    |     |");
+		System.out.println("  | |    |     |");
+		System.out.println("  | |    \\     /");
+		System.out.println("  ---     -----\n");
+	}
+	
+	public static void sun() {
+		System.out.println("     \\   /");
+		System.out.println("   --------");
+		System.out.println("\\ /        \\ /");
+		System.out.println("  | SUN :) |");
+		System.out.println("/ \\        / \\");
+		System.out.println("   --------");
+		System.out.println("     /   \\");
+		System.out.println();
+	}
+	
+	public static void popsicle() {
+		top();
+		bottom();
+	}
+	
+	public static void letterP() {
+		System.out.println("PPPPP");
+		System.out.println("P    P");
+		System.out.println("PPPPPP");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println();
+	}
+	
+	public static void letterO() {
+		System.out.println(" OOOO");
+		System.out.println("O    O");
+		System.out.println("O    O");
+		System.out.println("O    O");
+		System.out.println(" OOOO");
+		System.out.println();
+	}
+	
+	public static void letterS() {
+		System.out.println("SSSSSS");
+		System.out.println("S");
+		System.out.println("SSSSSS");
+		System.out.println("     S");
+		System.out.println("SSSSSS");
+		System.out.println();
+	}
+	
+	public static void letterI() {
+		System.out.println("IIIII");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("IIIII");
+		System.out.println();
+	}
+	
+	public static void letterL() {
+		System.out.println("L");
+		System.out.println("L");
+		System.out.println("L");
+		System.out.println("L");
+		System.out.println("LLLLLL");
+		System.out.println();
+	}
+	
+	public static void letterE() {
+		System.out.println("EEEEEE");
+		System.out.println("E");
+		System.out.println("EEEE");
+		System.out.println("E");
+		System.out.println("EEEEEE");
+		System.out.println();
+	}
+	public static void letterC() {
+		System.out.println("CCCCCC");
+		System.out.println("C    ");
+		System.out.println("C    ");
+		System.out.println("C    ");
+		System.out.println("CCCCCC");
+		System.out.println();
+	}
+}
